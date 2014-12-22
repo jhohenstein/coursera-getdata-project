a3b0e99e82a769870db6f1846431279025b94afe
Jeffrey Hohenstein <jeffrey.hohenstein@gmail.com>
2014-12-21 18:42:19 -0600

    Completed course. Missed deadline.

--- run_analysis.R
+++ run_analysis.R
@@ -29,7 +29,9 @@ loadData <- function() {
   data <- matrix(,nrow=0,ncol=561)
   for(f in files){
     message(paste("Loading file (",f,")",sep=""))
-    data <- rbind(data,read.fwf(f,rep(16,561),header = FALSE,buffersize = 10000))
+    data <- rbind(data,read.fwf(f,rep(16,561),header = FALSE))
+    message("Subject data set structure after loading:")
+    message(str(data))
   }
   
   data
@@ -54,6 +56,8 @@ loadSubjects <- function(){
   for(f in files){
     message(paste("Loading file (",f,")",sep=""))
     data <- rbind(data,read.csv(f,header = FALSE))
+    message("Subject data set structure after loading:")
+    message(str(data))
   }
   
   colnames(data) <- c("subjectId")
@@ -73,6 +77,8 @@ loadLabels <- function() {
   
   message(paste("Loading file (",fileName,")",sep=""))
   features <- read.csv(fileName,sep=" ",header=FALSE,stringsAsFactors=FALSE)
+  message("Label data set structure after loading:")
+  message(str(features))
   
   features[,2]
 }
@@ -82,6 +88,8 @@ loadLabels <- function() {
 # @return activities, a data frame with 2 columns, the first the PK, the second the label
 loadActivities <- function() {
   activities <- read.csv("data/UCI HAR Dataset/activity_labels.txt",sep=" ",header=FALSE,stringsAsFactors=FALSE)
+  message("Activity data set structure after loading:")
+  message(str(activities))
   colnames(activities) <- c("activityId","activityName")
   activities
 }
@@ -107,11 +115,35 @@ loadDataActivities <- function() {
   for(f in files){
     message(paste("Loading file (",f,")",sep=""))
     activities <- rbind(activities,read.csv(f,header = FALSE))
+    message("Activity data set structure after loading:")
+    message(str(activities))
   }
   
   colnames(activities) <- c("activityId")
   activities
   
+}
+
+# Summarize the data set grouping by Activity and Subject into a single 
+# "tidy" data set.
+#
+# @param data The data set to summarize
+# @return summarized data set grouped by Activity and Subject and with
+#         labels replaced to account for summarization
+createSummaryByActivityAndSubject <- function(data) {
+  table <- tbl_df(data)
+
+  # Group and summarize the data
+  summaryData <- table %>% group_by(activityId,activityName,subjectId) %>% summarise_each(funs(mean))
+  
+  # Relabel the summary data to enclose variable names in "mean"
+  metricColumns <- ! colnames(summaryData) %in% c("activityId","subjectId","activityName")
+  colnames(summaryData)[metricColumns] <- paste("mean('",colnames(summaryData)[metricColumns],"')",sep="")
+  
+  message("Summary data set dimensions:")
+  message(str(summaryData))
+  
+  summaryData
 }
 
 # Load the data, apply labels, and reduce the columns
@@ -164,4 +196,5 @@ main <- function() {
 }
 
 # Upon sourcing this file, run it
-main()
+# TODO
+# main()
